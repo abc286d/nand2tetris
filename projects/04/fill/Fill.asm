ad4b060581cfc560c1cc42d21acd96ddc9005cfc
abc286d <abc286d@gmail.com>
2023-08-02 20:19:17 -0700

    Finished chapter 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,119 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+// Method1:
+
+// while True:
+//     if key_input != 0:
+//         for i in range(255):
+//             set_black(i)
+//     else:
+//         for i in range(255):
+//             set_clear(i)
+
+// Method2:
+// while True:
+//     if key_input != 0:
+//         set_black(i)
+//         i+=1
+//     else:
+//         set_while(i)
+//         i-=1
+
+// Method2:
+//     @SCREEN
+//     D=A
+//     @curr
+//     M=D
+// (LOOP)
+//     // check the keyboard input
+//     @KBD
+//     D=M
+//     @CLEAR
+//     D;JEQ
+//     
+//     @curr
+//     D=M
+//     @24576
+//     D=D-A
+//     @LOOP
+//     D;JEQ
+// 
+//     @curr
+//     A=M
+//     M=-1
+//     @curr
+//     M=M+1
+//     
+//     @LOOP
+//     0;JMP
+// 
+// (CLEAR)
+//     @curr
+//     D=M
+//     @16383
+//     D=D-A
+//     @LOOP
+//     D;JEQ
+
+//     @curr
+//     A=M
+//     M=0
+//     @curr
+//     M=M-1
+// 
+//     @LOOP
+//     0;JMP
+ 
+// Method1:
+(LOOP)
+    // reset counter in the beginning of the loop
+    @SCREEN
+    D=A
+    @curr
+    M=D
+    @i
+    M=0
+
+    // check the keyboard input
+    @KBD
+    D=M
+    @CLEAR
+    D;JEQ
+
+(BLACK)
+    // loop 8192 times to print black
+    @curr
+    A=M
+    M=-1
+    @curr
+    M=M+1
+    @i
+    M=M+1
+    D=M
+    @8192
+    D=D-A
+    @BLACK
+    D;JNE
+
+    @LOOP
+    0;JMP
+
+(CLEAR)
+    // loop 8192 times to print white
+    @curr
+    A=M
+    M=0
+    @curr
+    M=M+1
+    @i
+    M=M+1
+    D=M
+    @8192
+    D=D-A
+    @CLEAR
+    D;JNE
+
+    @LOOP
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,82 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// sum = 0
+// pow = 1
+// base = R0
+// remain = R1
+// 
+// while remain != 0:
+//     if pow & R1 != 0:
+//         sum += base
+//         remain -= pow
+//     base += base
+//     pow += pow
+//      
+// R2 = sum
+
+    // initialize
+    @sum
+    M=0
+    @pow
+    M=1
+    @R0
+    D=M
+    @base
+    M=D
+    @R1
+    D=M
+    @remain
+    M=D
+
+(LOOP)
+    // while remain != 0
+    @remain
+    D=M
+    @RET
+    D;JEQ
+
+(CHECK)
+    // check whether to add
+    @pow
+    D=M
+    @R1
+    D=D&M
+
+    // jump to INC if so
+    @INC
+    D;JEQ
+
+    // update sum and remain if checks are met
+    @base
+    D=M
+    @sum
+    M=D+M
+    @pow
+    D=M
+    @remain
+    M=M-D
+
+(INC)
+    // increase base and pow
+    @base
+    D=M
+    M=D+M
+    @pow
+    D=M
+    M=D+M
+
+    // go to loop
+    @LOOP
+    0;JMP
+
+(RET)
+    @sum
+    D=M
+    @R2
+    M=D
+
+(END)
+    @END
+    0;JMP
